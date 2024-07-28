296263918d56dc3623cd8a4bbbe3c168fec6d086
Dominik Hucka <dominikhucka@googlemail.com>
2024-07-28 21:50:48 +0200

    added card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Header from './components/header';
 import MainPage from './components/mainPage';
+import { AnyMxRecord } from 'dns';
 
 
 interface AppProps {
@@ -9,6 +10,8 @@ interface AppProps {
 
 interface AppState {
     weatherData: any;
+    isLoading: boolean;
+    error: any;
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -16,23 +19,42 @@ class App extends React.Component<AppProps, AppState> {
 
 
     state: AppState = {
-        weatherData: undefined
+        weatherData: undefined,
+        isLoading: false,
+        error: null,
     }
 
 
-    componentDidMount = () => {
+    componentDidMount = async () => {
         this.fetchApiData();
     }
 
 
-    fetchApiData() {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=reutlingen&appid=${this.apiKey}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log('weahter api', data)
-                this.setState({ weatherData: data })
-            })
-            .catch(error => console.error('Fetching Failed', error));
+    async fetchApiData() {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=reutlingen&appid=${this.apiKey}`;
+        try {
+            let res = await fetch(url);
+            if (!res.ok) {
+                throw new Error(`Fehler: ${res.status} ${res.statusText}`);
+            }
+            let resAsJSON = await res.json();
+            console.log('current Weather Data', resAsJSON);
+            this.setState({ weatherData: resAsJSON })
+        } catch (error: any) {
+            console.error('ERROR', error);
+            this.setState({ error: error.message })
+        } finally {
+            this.setState({ isLoading: false })
+        }
+
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log('weahter api', data)
+        //         this.setState({ weatherData: data })
+        //     })
+        //     .catch(error => console.error('Fetching Failed', error));
+
     }
 
 
@@ -41,7 +63,7 @@ class App extends React.Component<AppProps, AppState> {
 
         return <React.Fragment>
             <Header />
-            <MainPage weatherData={weatherData} />
+            <MainPage currentWeather={weatherData} />
         </React.Fragment>;
     }
 }
--- src/components/mainPage.tsx
+++ src/components/mainPage.tsx
@@ -1,24 +1,30 @@
 import * as React from 'react';
+import '../css/mainPage.css';
 
 
 interface MainPageProps {
-    weatherData: any;
+    currentWeather?: any;
 }
 
 interface MainPageState {
-    
+
 }
 
 
 class MainPage extends React.Component<MainPageProps, MainPageState> {
-    state = { }
+    state = {}
 
 
     render() {
-        const {weatherData} = this.props
+        const { currentWeather } = this.props
 
-    
-        return <h2>{weatherData.name}</h2>
+
+        return <div className="card" style={{width: "18rem"}}>
+            <div className="card-body">
+                <h5 className="card-title">{currentWeather.name}</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
     }
 }
 
