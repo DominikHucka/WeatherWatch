81e169918a51632333f4c79693ac8895528baac2
Dominik Hucka <dominikhucka@googlemail.com>
2024-07-29 20:46:00 +0200

    fixed fetch data bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Header from './components/header';
 import MainPage from './components/mainPage';
-import { AnyMxRecord } from 'dns';
 
 
 interface AppProps {
@@ -16,7 +15,8 @@ interface AppState {
 
 class App extends React.Component<AppProps, AppState> {
     apiKey = process.env.REACT_APP_API;
-
+    location = 'manhatten';
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${this.apiKey}`;
 
     state: AppState = {
         weatherData: undefined,
@@ -31,30 +31,22 @@ class App extends React.Component<AppProps, AppState> {
 
 
     async fetchApiData() {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=reutlingen&appid=${this.apiKey}`;
-        try {
-            let res = await fetch(url);
-            if (!res.ok) {
-                throw new Error(`Fehler: ${res.status} ${res.statusText}`);
-            }
-            let resAsJSON = await res.json();
-            console.log('current Weather Data', resAsJSON);
-            this.setState({ weatherData: resAsJSON })
-        } catch (error: any) {
-            console.error('ERROR', error);
-            this.setState({ error: error.message })
-        } finally {
-            this.setState({ isLoading: false })
-        }
+        fetch(this.url)
+            .then(res => res.json())
+            .then(data => {
+                console.log('weahter api', data)
+                this.setState({
+                    weatherData: data,
+                    isLoading: false,
+                })
 
-        // fetch(url)
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log('weahter api', data)
-        //         this.setState({ weatherData: data })
-        //     })
-        //     .catch(error => console.error('Fetching Failed', error));
-
+            })
+            .catch(error => {
+                this.setState( {
+                    isLoading: false,
+                    error,
+                })
+            });
     }
 
 
--- src/components/mainPage.tsx
+++ src/components/mainPage.tsx
@@ -18,9 +18,13 @@ class MainPage extends React.Component<MainPageProps, MainPageState> {
     render() {
         const { currentWeather } = this.props
 
+        if (!currentWeather) {
+            return <div>Loading...</div>;
+        }
 
-        return <div className="card" style={{width: "18rem"}}>
+        return <div className="card" style={{ width: "18rem" }}>
             <div className="card-body">
+                <p>{currentWeather.weather[0].description}</p>
                 <h5 className="card-title">{currentWeather.name}</h5>
                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
